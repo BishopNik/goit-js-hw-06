70682e3187c60c740dc68bebbfb63b463f9106a8
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-07-10 22:08:59 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,33 @@
+const refForm = document.querySelector('.login-form');
 
+refForm.addEventListener('submit', onFormSubmit);
+
+const formValue = {};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const inputEmailValue = this.elements.email.value;
+    const inputPasswordValue = this.elements.password.value;
+
+    if (!inputEmailValue || !inputPasswordValue) {
+        alert('Все поля должны быть заполнены!');
+        return
+    }
+
+    // const formData = new FormData(event.currentTarget);
+    
+    // formData.forEach((value, key) => {
+    //     formValue[key] = value;
+    // });
+    
+    // formValue.email = inputEmailValue;
+    // formValue.password = inputPasswordValue;
+
+    formValue.email = inputEmailValue;
+    formValue.password = inputPasswordValue;
+
+    console.table(formValue);
+
+    refForm.reset();
+}
