88360432db73f6800357ffd121a72c0e1b888bbb
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-07-11 10:06:19 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  spanColorEl: document.querySelector('.color'),
+  btnEl: document.querySelector('.change-color'),
+}
+
+refs.btnEl.addEventListener('click', onChangeCololBody);
+
+function onChangeCololBody(event) {
+  const colorBody = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = colorBody;
+  refs.spanColorEl.textContent = colorBody;
+}
