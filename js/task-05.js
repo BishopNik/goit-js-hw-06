00e34a314d22758a7dd97ad5105ecff04f356d8a
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-07-28 11:00:51 +0200

    task-05_add trim

--- js/task-05.js
+++ js/task-05.js
@@ -1,15 +1,14 @@
 const refs = {
-    nameInput: document.querySelector('#name-input'),
-    textInput: document.querySelector('#name-output'),
+  nameInput: document.querySelector('#name-input'),
+  textInput: document.querySelector('#name-output'),
 };
 
 refs.nameInput.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-    if (event.currentTarget.value) {
-        refs.textInput.textContent = event.currentTarget.value
-    } else {
-        refs.textInput.textContent = 'Anonymous'
-    }
-    
+  if (event.currentTarget.value.trim()) {
+    refs.textInput.textContent = event.currentTarget.value;
+  } else {
+    refs.textInput.textContent = 'Anonymous';
+  }
 }
