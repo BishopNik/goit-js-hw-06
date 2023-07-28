cf6bb2da1202f58556627885d95cf41f63708173
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-07-28 14:08:45 +0200

    task-06_add trim

--- js/task-06.js
+++ js/task-06.js
@@ -3,12 +3,12 @@ const refInput = document.querySelector('#validation-input');
 refInput.addEventListener('blur', onValidationInput);
 
 function onValidationInput(event) {
-    const inputLength = Number(refInput.dataset.length);
-    if (event.currentTarget.value.length === inputLength) {
-        refInput.classList.remove('invalid');
-        refInput.classList.add('valid');        
-    } else {
-        refInput.classList.remove('valid');
-        refInput.classList.add('invalid');
-    }
+  const inputLength = Number(refInput.dataset.length);
+  if (event.currentTarget.value.trim().length === inputLength) {
+    refInput.classList.remove('invalid');
+    refInput.classList.add('valid');
+  } else {
+    refInput.classList.remove('valid');
+    refInput.classList.add('invalid');
+  }
 }
